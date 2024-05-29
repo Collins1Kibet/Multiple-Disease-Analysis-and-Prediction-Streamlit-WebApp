cce6d32df527ba4296646a41727f4f51d77b2f60
Collins1Kibet <collinskibet000@gmail.com>
2024-05-29 17:24:54 +0300

    Update Multiple_Disease_Prediction.py

--- Multiple_Disease_Prediction.py
+++ Multiple_Disease_Prediction.py
@@ -1,12 +1,15 @@
+import os
 import pickle
 import streamlit as st
 from streamlit_option_menu import option_menu
 
-allergy_cold_covid_or_Flu_Model = pickle.load(open("Trained_Model_Allergy_Cold_Covid_or_Flu.sav", 'rb'))
-diabetes_model = pickle.load(open("Trained_Model_Diabetes.sav", "rb"))
-heart_disease_model = pickle.load(open("Trained_Model_heart_disease.sav", "rb"))
-parkisons_disease_model = pickle.load(open("Trained_Model_Parkinsons_Disease.sav", 'rb'))
-breast_cancer_model = pickle.load(open("Trained_Model_Breast_Cancer.sav", "rb"))
+pwd = os.getcwd()
+
+allergy_cold_covid_or_Flu_Model = pickle.load(open(pwd + "/Trained_Model_Allergy_Cold_Covid_or_Flu.sav", 'rb'))
+diabetes_model = pickle.load(open(pwd + "/Trained_Model_Diabetes.sav", "rb"))
+heart_disease_model = pickle.load(open(pwd + "/Trained_Model_heart_disease.sav", "rb"))
+parkisons_disease_model = pickle.load(open(pwd + "/Trained_Model_Parkinsons_Disease.sav", 'rb'))
+breast_cancer_model = pickle.load(open(pwd + "/Trained_Model_Breast_Cancer.sav", "rb"))
 
 
 st.set_page_config(
